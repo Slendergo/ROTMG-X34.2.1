f9817605f73b84e51e00ad036dd50844f89a906a
Slendergo <Survivalistrivalist@gmail.com>
2023-02-17 00:08:27 +0000

    Configuration class & Json File

--- Server/SGB.GameServer/Core/Configuration.cs
+++ Server/SGB.GameServer/Core/Configuration.cs
@@ -63,21 +63,57 @@ namespace SGB.GameServer.Core
 
     public sealed class ConfigurationData
     {
-        public IOConfigurationData IOConfiguration { get; set; }
-        public SessionConfigurationData SessionConfiguration { get; set; }
+        public ServerConfiguration ServerConfiguration { get; set; }
+        public IOConfiguration IOConfiguration { get; set; }
+        public AnnouncerConfiguration AnnouncerConfiguration { get; set; }
+        public AutomatedRestartConfiguration AutomatedRestartConfiguration { get; set; }
+        public ChatConfiguration ChatConfiguration { get; set; }
+        public SessionConfiguration SessionConfiguration { get; set; }
         public LoggingConfiguration LoggingConfiguration { get; set; }
         public RealmConfiguration RealmConfiguration { get; set; }
         public TestingConfiguration TestingConfiguration { get; set; }
     }
 
-    public sealed class IOConfigurationData
+    public sealed class ServerConfiguration
     {
-        public int StartingSessionCacheCount { get; set; }
-        public int Port { get; set; }
-        public int Backlog { get; set; }
+        public string BuildVersion { get; set; }
     }
 
-    public sealed class SessionConfigurationData
+    public sealed class IOConfiguration
+    {
+        public int StartingSessionCacheCount { get; set; }
+        public int Backlog { get; set; }
+        public int Port { get; set; }
+    }
+
+    public sealed class AnnouncerConfiguration
+    {
+        public bool CyclicAnnouncements { get; set; }
+        public int RarelyPeriodMinutes { get; set; }
+        public string[] Rarely { get; set; }
+        public int OftenPeriodMinutes { get; set; }
+        public string[] Periodically { get; set; }
+        public int FrequentPeriodMinutes { get; set; }
+        public string[] Frequent { get; set; }
+    }
+
+    public sealed class AutomatedRestartConfiguration
+    {
+        public int HoursBeforeRestart { get; set; }
+        public string RestartMessage15Minutes { get; set; }
+        public string RestartMessage30Seconds { get; set; }
+        public string RestartMessage15Seconds { get; set; }
+        public string RestartMessage5Seconds { get; set; }
+    }
+
+    public sealed class ChatConfiguration
+    {
+        public int SpamTimeoutMS { get; set; }
+        public int SpamThreshold { get; set; }
+        public int StarRequirement { get; set; }
+    }
+
+    public sealed class SessionConfiguration
     {
         public int MaxSessions { get; set; }
     }
@@ -94,17 +130,17 @@ namespace SGB.GameServer.Core
         public int StartingRealms { get; set; }
     }
 
-    public class TestingConfiguration
+    public sealed class TestingConfiguration
     {
         public int StartingLevel { get; set; }
         public int StartingHealthPotions { get; set; }
         public int StartingMagicPotions { get; set; }
         public int PermanentFameAmount { get; set; }
-        public int PermanentCreditAmount { get; set; }
-        public List<StartingItem> StartingItems { get; set; }
+        public int PermanentCreditsAmount { get; set; }
+        public StartingItem[] StartingItems { get; set; }
     }
 
-    public class StartingItem
+    public sealed class StartingItem
     {
         public string IdName { get; set; }
         public string Weapon { get; set; }
